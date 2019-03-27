a6471a9dede1bf53bb440bbbfa416871378c252b
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-03-26 20:48:56 -0500

    create actions complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,80 @@
-export function increaseCounter(){
+export const increaseCounter = () => {
   return {
-    type:"INCREASE_COUNTER"
-  }
-}
+    type: "INCREASE_COUNTER"
+  };
+};
 
-export function setSpecialText(txt){
+export const decreaseCounter = () => {
   return {
-    type:"SET_SPECIAL_TEXT",
-    value:txt
-  }
-}
+    type: "DECREASE_COUNTER"
+  };
+};
 
-export function addUser(user){
+export const setSpecialText = txt => {
   return {
-    type:"ADD_USER",
-    value:user
-  }
-}
-export function removeUser(){
+    type: "SET_SPECIAL_TEXT",
+    value: txt
+  };
+};
+
+export const removeUser = () => {
   return {
-    type:"REMOVE_USER"
-  }
-}
+    type: "REMOVE_USER"
+  };
+};
+
+export const addUser = user => {
+  return {
+    type: "ADD_USER",
+    value: user
+  };
+};
+
+export const setSearchText = text => {
+  return {
+    type: "SET_SEARCH_TEXT",
+    value: text
+  };
+};
+
+export const setIsLoading = isLoading => {
+  return {
+    type: "SET_IS_LOADING",
+    value: isLoading
+  };
+};
+
+export const setTemp = temp => {
+  return {
+    type: "SET_TEMP",
+    value: temp
+  };
+};
+
+export const setCurrentCity = city => {
+  return {
+    type: "SET_CURRENT_CITY",
+    value: city
+  };
+};
+
+export const setVideoURL = URL => {
+  return {
+    type: "SET_VIDEO_URL",
+    value: URL
+  };
+};
+
+export const setCurrentUserSort = sort => {
+  return {
+    type: "SET_CURRENT_USER_SORT",
+    value: sort
+  };
+};
+
+export const setVideoScale = scale => {
+  return {
+    type: "SET_VIDEO_SCALE",
+    value: scale
+  };
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,94 @@
-
-function currentCount(state=0, action){
-  if(action.type === "INCREASE_COUNTER"){
-    
+const currentCount = (state = 0, action) => {
+  if (action.type === "INCREASE_COUNTER") {
+    return {
+      count: state.count + 1
+    };
   }
-  if(action.type === "DECREASE_COUNTER"){
-    
-  }
-  return state;
-}   
-
-function users(state =[], action){
-  if(action.type === "ADD_USER"){
-
-  }
-  if(action.type === "REMOVE_USER"){
-    
+  if (action.type === "DECREASE_COUNTER") {
+    return {
+      count: state.count - 1
+    };
   }
   return state;
-}
+};
 
-
-function specialText(state = "", action){
-  if(action.type === "SET_SPECIAL_TEXT"){
+const specialText = (state = "", action) => {
+  if (action.type === "SET_SPECIAL_TEXT") {
     return action.value;
   }
   return state;
-}
+};
 
+const currentCity = (state = "", action) => {
+  if (action.type === "SET_CURRENT_CITY") {
+    return action.value;
+  }
+  return state;
+};
+
+const users = (state = [], action) => {
+  if (action.type === "ADD_USER") {
+    return [...state, action.value];
+  }
+  if (action.type === "REMOVE_USER") {
+    return state.slice(1);
+  }
+  return state;
+};
+
+const currentTemp = (state = 0, action) => {
+  if (action.type === "SET_TEMP") {
+    return action.value;
+  }
+  return state;
+};
+
+const isLoading = (state = false, action) => {
+  if (action.type === "SET_IS_LOADING") {
+    return action.value;
+  }
+  return state;
+};
+
+const videoURL = (state = "", action) => {
+  if (action.type === "SET_VIDEO_URL") {
+    return action.value;
+  }
+  return state;
+};
+
+const searchText = (state = "", action) => {
+  if (action.type === "SET_SEARCH_TEXT") {
+    return action.value;
+  }
+  return state;
+};
+
+const currentUserSort = (state = "first_name", action) => {
+  if (action.type === "SET_SEARCH_TEXT") {
+    return action.value;
+  }
+  return state;
+};
+
+const videoScale = (state = 1, action) => {
+  if (action.type === "SET_VIDEO_SCALE") {
+    return action.value;
+  }
+  return state;
+};
+
+const rootReducer = combineReducers({
+  currentCount,
+  specialText,
+  currentCity,
+  searchText,
+  currentTemp,
+  isLoading,
+  videoURL,
+  currentUserSort,
+  videoScale,
+  users
+});
+
+export default rootReducer;
