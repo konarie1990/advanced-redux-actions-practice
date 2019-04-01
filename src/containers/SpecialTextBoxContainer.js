c46d9fdee60740c5021e66f1dc7f35c0940e47b2
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-04-01 00:20:09 -0500

    working on containers

--- src/containers/SpecialTextBoxContainer.js
+++ src/containers/SpecialTextBoxContainer.js
@@ -1,11 +1,14 @@
-import { connect } from 'react-redux';
-import {setSpecialText} from "../actions";
+import { connect } from "react-redux";
+import { setSpecialText } from "../actions";
 import SpecialTextBox from "../components/SpecialTextBox";
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    set: (txt) => dispatch(setSpecialText(txt))
-  }
-}
+    set: txt => dispatch(setSpecialText(txt))
+  };
+};
 
-export default connect(null, mapDispatchToProps)(SpecialTextBox);
+export default connect(
+  null,
+  mapDispatchToProps
+)(SpecialTextBox);
